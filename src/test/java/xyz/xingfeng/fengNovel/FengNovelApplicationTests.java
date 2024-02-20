2bfc2feb6595077d0181a66a5db56784ae83f7c3
Administrator <2695570953@qq.com>
2024-02-20 15:12:09 +0800

    test

---
+++ src/test/java/xyz/xingfeng/fengNovel/FengNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xingfeng.fengNovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FengNovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
